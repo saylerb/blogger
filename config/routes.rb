48f13a55fa53ead753405a51215007746eee1bf4
saylerb <sayler.b@gmail.com>
2016-05-25 23:14:43 -0600

    generate comments controller and add create comment logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :articles #, only: [:index]
+  resources :articles do #, only: [:index]
+    resources :comments
+  end
 
   root to: 'articles#index'
 end
