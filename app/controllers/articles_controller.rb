5bd318248c894ae68ac68c5c065ced56417f8fd7
saylerb <sayler.b@gmail.com>
2016-05-25 21:03:07 -0600

    add helper method for article parameters

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  include ArticlesHelper
+
   def index 
     @articles = Article.all
   end
@@ -13,11 +15,8 @@ class ArticlesController < ApplicationController
   end
   
   def create
-    @article = Article.new
-    @article.title = params[:article][:title]
-    @article.body = params[:article][:body]
+    @article = Article.new(article_params)
     @article.save
-    
     redirect_to article_path(@article)
   end
   
